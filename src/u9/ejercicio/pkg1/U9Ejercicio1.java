6167ae8c035f63cb5395dcdabea540620b7cc5a7
Elia <eliamariapalavecino@gmail.com>
2022-04-06 15:37:03 -0300

    Se agrega la consigna

--- src/u9/ejercicio/pkg1/U9Ejercicio1.java
+++ src/u9/ejercicio/pkg1/U9Ejercicio1.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Tenemos una clase padre Animal junto con sus 3 clases hijas Perro, Gato, Caballo.
+La clase Animal tendra como atributos el nombre, alimento, edad y raza del Animal.
+Crear un metodo en la clase Animal a traves del cual cada clase hija debera mostrar luego un mensaje por pantalla informando de que se alimenta.La clase Main esta determinado.   
  */
 package u9.ejercicio.pkg1;
 
